33d9c394f3a3ba38aa3e6120bdf7c83bbe1ce5a2
Andrii Zaika <lytiy7404@gmail.com>
2026-09-11 20:54:33 +0200

    Add ShipTypes

--- Battleship with Java/task/src/battleship/Main.java
+++ Battleship with Java/task/src/battleship/Main.java
@@ -6,7 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
         Battlefield battlefield = new Battlefield();
-        battlefield.toString();
         Scanner scanner = new Scanner(System.in);
         System.out.println(battlefield.toString());
         System.out.println("Enter the coordinates of the ship:");
